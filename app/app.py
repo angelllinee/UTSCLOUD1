170f44c113fb6db7191d700483ecc1db03092bb6
Angeline Apriliana <angeline.apriliana@mhs.itenas.ac.id>
2025-04-27 12:41:12 +0700

    First commit

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pymysql
+
+app = Flask(__name__)
+
+# Database connection
+db = pymysql.connect(
+    host='ecom-db.chm448q8s314.ap-southeast-1.rds.amazonaws.com',
+    user='admin',
+    password='angeline117!',
+    database='ecom-db'
+)
+
+@app.route('/')
+def index():
+    cursor = db.cursor()
+    cursor.execute("SELECT product_name, price, image_url FROM products")
+    products = cursor.fetchall()
+    return render_template('index.html', products=products)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
